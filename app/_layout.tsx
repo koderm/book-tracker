022b4e62535c0be59eabbca93bf37773ff0ca175
unknown <koder.mustafa@hotmail.com>
2025-06-29 19:32:04 +1000

    define initial route index for expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { IconSymbol } from "../components/ui/IconSymbol";
 
 const RootLayout = () => (
   <Tabs>
-    <Tabs.Screen name="MyBooks" options={{ title: "My Books", tabBarIcon: ({ color }) => <IconSymbol size={28} name="books.vertical.fill" color={color}/> }} />
+    <Tabs.Screen name="index" options={{ title: "My Books", tabBarIcon: ({ color }) => <IconSymbol size={28} name="books.vertical.fill" color={color}/> }} />
     <Tabs.Screen name="browse" options={{ title: "Browse", headerShown: false, tabBarIcon: ({ color }) => <IconSymbol size={28} name="magnifyingglass" color={color}/> }} />
   </Tabs>
 );
