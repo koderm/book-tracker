ae552ba2713d4f196a86b557fee5ae60d708e868
unknown <koder.mustafa@hotmail.com>
2025-06-29 12:44:21 +1000

    renamed screen components to use camel case

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
 import { Tabs } from "expo-router";
-import { IconSymbol } from "./components/IconSymbol";
+import { IconSymbol } from "../components/ui/IconSymbol";
 
 const RootLayout = () => (
   <Tabs>
     <Tabs.Screen
-      name="home"
+      name="Home"
       options={{
         title: 'Home',
         tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
       }}
     />
-    <Tabs.Screen name="my-books" options={{ title: "My Books" }} />
-    <Tabs.Screen name="browse" options={{ title: "Browse" }} />
+    <Tabs.Screen name="MyBooks" options={{ title: "My Books" }} />
+    <Tabs.Screen name="Browse" options={{ title: "Browse" }} />
   </Tabs>
 );
 
